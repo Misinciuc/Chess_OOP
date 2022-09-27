3f67a4cb373fc78cd4a6b270a1bd946d9758663c
Iurie <iuriemisinciuc18@gmail.com>
2022-09-26 20:34:12 -0400

    render Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import BoardComponent from "./components/BoardComponent";
+import { useEffect, useState } from "react";
+import { Board } from "./models/Board";
 
 const App = () => {
-  return <div className="app"></div>;
+  const [board, setBoard] = useState(new Board());
+
+  useEffect(() => {
+    restart();
+  }, []);
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initializeCells();
+    setBoard(newBoard);
+  }
+
+  return (
+    <div className="app">
+      <BoardComponent board={board} setBoard={setBoard} />
+    </div>
+  );
 };
 
 export default App;
